ecc1c933d4aeb182d3902acbec1ef599136dd937
Matheus Hans Duffeck <69155648+Hans-Oliveira@users.noreply.github.com>
2025-03-13 15:03:49 -0300

    .
    .

---
+++ view/home.php
@@ -0,0 +1,24 @@
+
+<div class="container d-flex justify-content-center w-50 mt-5">
+    <div id="carouselExampleIndicators" class="carousel slide w-50" data-ride="carousel" data-interval="false" >
+        <ol class="carousel-indicators m-0">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+        </ol>
+        <div class="carousel-inner">
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
---
+++ view/login.php
@@ -0,0 +1,20 @@
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <img src="https://img.icons8.com/color/200/star-wars.png" class="mx-auto d-block" alt="User Icon">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Login" id="user" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Senha" id="pass" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary d-block mx-auto" id="login">Entrar</button>
+                    <a class="d-flex justify-content-end mt-1" data-toggle="modal" data-target="#editUser" style="cursor: pointer;">Criar usuário</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
